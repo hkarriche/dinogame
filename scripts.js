5ad7f8a4832b6c091754bf450ffa0805e476a354
Karriche Hakim <asgazib2@gmail.com>
2021-03-03 10:18:01 +0100

    add setInterval

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+
 const dino = document.getElementById("dino");
 function jump(){
     if(dino.classList != "jump"){
@@ -8,6 +10,17 @@ function jump(){
     }
 
 }
+
+let isAlive = setInterval(function(){
+    
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top")) ;
+    let cactusposition = parseInt(window.getComputedStyle(cactus).getPropertyValue("left")) ;
+    console.log(cactusposition);
+if(cactusposition<50 && cactusposition >0 && dinoTop >=140 ){
+    alert('Game Over');
+}
+
+  },10);
 
 document.addEventListener("keydown",function(event){
     jump();
